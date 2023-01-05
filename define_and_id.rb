8b7a559157c0dc47193a78b9cfa6e99294691133
Kelly <119141863+k-leahy@users.noreply.github.com>
2023-01-05 11:25:44 -0700

    Define methods

--- define_and_id.rb
+++ define_and_id.rb
@@ -9,19 +9,28 @@
 
 # YOU DO the rest:
 
-
 "Hello World".include?("Hello")
-
+# The include method is called on the string object "Hello World"
+# The job of this method is to see if the string contains the ("word") in this case ("Hello")
+# The return value is true
 
 "Hello World".end_with?("Hello")
-
+# The end_with method is called on the string object "Hello World"
+# The job of this method is to check if the string ends with the ("word") in this case ("Hello")
+# The return value is false
 
 "Hello World".end_with?("rld")
-
+# The end_with method is called on the string object "Hello World"
+# The job of this method is to check if the string ends with the ("letters") in this case ("rld")
+# The return value is true
 
 12.even?
-
+# The even method is called on the integer 12
+# The job of this method is to determine if the integer is even
+# The return value is true
 
 18.next
-
+# The next method is called on the integer 18
+# The job of this method is to determine the integer that would come after the one listed
+# The return value is 19
 
